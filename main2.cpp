cfc8a1b271fe16e4b49a8f01fbb405bc7c45db50
santika006 <72698209+santika006@users.noreply.github.com>
2021-03-01 14:23:16 +0700

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+char huruf [5];
+cout<<"Massukkan huruf : 'P','E','N','S','I','T'";
+cout<<endl;
+
+cout<<endl;
+	return 0;
+}
